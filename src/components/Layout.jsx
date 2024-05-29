c39aa14b9c60f53fd0a3db675b925fc949b59a3a
Ashish Nagar <Ashishnagar033@gmail.com>
2024-05-29 09:48:42 +0530

    minor code update

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,7 +4,7 @@ import NavBar from "./NavBar";
 const Layout = () => {
   return (
     <div id="layout" className="bg-[#f7f9fb] flex-1 flex-column ">
-      <h1 className="py-4 text-2xl font-bold text-[#4e42d4] text-center">
+      <h1 className="py-4  text-2xl font-bold text-[#4e42d4] text-center">
         Movies App
       </h1>
       <NavBar />
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -13,10 +13,10 @@ const NavBar = () => {
       <NavLink
         to="/"
         className={({ isActive }) =>
-          `flex-1 text-center p-4 text-xl ${
+          `flex-1 text-center p-4 text-xl  ${
             isActive
               ? "border-b-4  font-bold border-slate-600 text-[#4e42d4] bg-slate-400"
-              : "text-[#f7f9fb]  font-semibold"
+              : "text-black  font-semibold"
           }
            hover:text-[#4e42d4] 
            hover:font-bold 
@@ -32,10 +32,10 @@ const NavBar = () => {
       <NavLink
         to="/fav"
         className={({ isActive }) =>
-          `flex-1 text-center text-xl   p-4 ${
+          `flex-1 text-center text-xl  p-4 ${
             isActive
               ? "text-[#4e42d4]  font-bold bg-slate-400 border-b-4 border-slate-600 "
-              : "text-[#f7f9fb]  font-semibold "
+              : "text-black  font-semibold "
           }
           
            hover:text-[#4e42d4] 
