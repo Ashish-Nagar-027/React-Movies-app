bf3fa1420ee0610e9b8f7ee884031aed3803ddd3
Ashish Nagar <Ashishnagar033@gmail.com>
2024-05-29 09:43:01 +0530

    style and code update

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,28 +1,52 @@
+import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
+
+  const favoriteMovies = useSelector((state) => state.FavMoviesData.FavMovies);
+  
+
+
+
   return (
-    <div className="flex flex-row mb-5  justify-around bg-slate-400 p-4">
-      <div>
-        <NavLink
-          to="/"
-          className={({ isActive }) =>
-            isActive ? "text-red-400" : "text-white"
+    <div className="flex flex-row mb-5  bg-slate-300 ">
+      <NavLink
+        to="/"
+        className={({ isActive }) =>
+          `flex-1 text-center p-4 text-xl ${
+            isActive
+              ? "border-b-4  font-bold border-slate-600 text-[#4e42d4] bg-slate-400"
+              : "text-[#f7f9fb]  font-semibold"
           }
-        >
-          Movies
-        </NavLink>
-      </div>
-      <div>
-        <NavLink
-          to="/fav"
-          className={({ isActive }) =>
-            isActive ? "text-red-400" : "text-white"
+           hover:text-[#4e42d4] 
+           hover:font-bold 
+        
+           hover:border-b-4
+          
+          `
+        }
+      >
+        Movies
+      </NavLink>
+
+      <NavLink
+        to="/fav"
+        className={({ isActive }) =>
+          `flex-1 text-center text-xl   p-4 ${
+            isActive
+              ? "text-[#4e42d4]  font-bold bg-slate-400 border-b-4 border-slate-600 "
+              : "text-[#f7f9fb]  font-semibold "
           }
-        >
-          Favorite Movies
-        </NavLink>
-      </div>
+          
+           hover:text-[#4e42d4] 
+           hover:font-bold 
+           hover:border-b-4
+         
+          `
+        }
+      >
+        Favorite Movies ({favoriteMovies?.length})
+      </NavLink>
     </div>
   );
 };
