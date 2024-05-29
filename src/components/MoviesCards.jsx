996df6ef489ec81f666961ffcf170f8979d49f19
Ashish Nagar <Ashishnagar033@gmail.com>
2024-05-29 09:42:30 +0530

    skeleton style update

--- src/components/MoviesCards.jsx
+++ src/components/MoviesCards.jsx
@@ -1,13 +1,20 @@
 /* eslint-disable react/prop-types */
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import MovieCard from "../components/MovieCard";
 import { useDispatch, useSelector } from "react-redux";
+
+
 import { setMoviesData } from "../features/MoviesSlice";
+import Skeletons from "./Skeletons";
+
+
+
 
 const MoviesCards = ({ allmovies=true }) => {
   const dispatch = useDispatch();
   const moviesData = useSelector((state) => state.MoviesData.movies);
   const favoriteMovies = useSelector((state) => state.FavMoviesData.FavMovies);
+  const [loading , setLoading] = useState(true)
 
   // sorting the movies from high to low ratting
   const sortByRating = (moviesArray) => {
@@ -23,22 +30,24 @@ const MoviesCards = ({ allmovies=true }) => {
       .then((response) => response.json())
       .then((json) => sortByRating(json))
       .then((moviesData) => {
+        setLoading(false);
         dispatch(setMoviesData(moviesData));
       });
   }, []);
 
   return (
-    <div className=" p-5 flex justify-center gap-2 flex-wrap">
-      {
-      allmovies ? (
+    <div className="  p-5 flex justify-center gap-4 flex-wrap">
+     {  loading &&  <Skeletons /> }   
+
+      {allmovies ? (
         moviesData &&
-        moviesData.map((movieData) => {
+        moviesData?.map((movieData) => {
           return <MovieCard key={movieData.id} movieData={movieData} />;
         })
       ) : moviesData && favoriteMovies.length === 0 ? (
         <h1>You have not added any favorite movies</h1>
       ) : (
-        moviesData.map((movieData) => {
+        moviesData?.map((movieData) => {
           return (
             favoriteMovies.includes(movieData.id) && (
               <MovieCard key={movieData.id} movieData={movieData} />
